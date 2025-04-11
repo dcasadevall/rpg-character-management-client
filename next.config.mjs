08f1a5b9511d2cd426b379d07f9142ff56594e22
Daniel Casadevall Pino <dcasadevall@me.com>
2025-04-11 07:40:29 -0700

    Allow images from vercel

--- next.config.mjs
+++ next.config.mjs
@@ -2,6 +2,17 @@
 const nextConfig = {
     images: {
         domains: ['localhost'],
+        remotePatterns: [
+            {
+                protocol: 'https',
+                hostname: '**.vercel.app',
+            },
+            {
+                protocol: 'http',
+                hostname: 'localhost',
+                port: '3000',
+            },
+        ],
     },
     // Allow images from the localhost domain for development
     reactStrictMode: true,
